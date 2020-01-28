edbf9ab0915c0128b5284c4ee1fd004e60bcf86d
Tim Whiting <tim@whitings.org>
2020-01-27 21:15:47 -0700

    updates to the mcts generator with TODOs and stubbed out evaluation functions

--- generators/mcts_generator.py
+++ generators/mcts_generator.py
@@ -7,8 +7,15 @@ from webscraping.web_scraper import *
 class CSS():
     def __init__(self, rules):
         self.rules = rules
+    
+    def evaluate(self):
+        # TODO: Evaluate the rules based on some criteria
+        return 1
 
 class WebSite():
+    photo_eval_limit = .5
+    css_eval_limit = .7
+
     def __init__(self, url=None, html=None):
         if html is not None:
             self.html = html
@@ -16,7 +23,27 @@ class WebSite():
         else:
             self.html, css = scrape(url)
             self.css = CSS(rules=css)
-    
+
+    def gen_photo(self):
+        # TOOD: Generate photo from HTML & the new CSS
+        return None
+
+    def evaluate_photo(self):
+        photo = self.gen_photo()
+        # TODO: Evaluate a photo based on some criteria
+        return 1
+
+    def evaluate_css(self):
+        return self.css.evaluate()
+
+    def evaluate(self):
+        css_evaluation = self.evaluate_css()
+        if css_evaluation < WebSite.css_eval_limit:
+            return 0
+        else:
+            photo_evaluation = self.evaluate_photo()
+            return css_evaluation*.2 + photo_evaluation*.8
+
     def __str__(self):
         return str((self.html, self.css))
 
@@ -27,8 +54,9 @@ class WebSite():
         ws = WebSite(html=self.html)
         ws.css = copy(self.css)
         return ws
-
+           
 class WebSiteState():
+    min_acceptable_evaluation = .5
     def __init__(self, url):
         self.website = WebSite(url=url)
         self.depth = 1
@@ -43,13 +71,13 @@ class WebSiteState():
         return newState
 
     def isTerminal(self):
-        return self.depth == 5
+        return self.website.evaluate() < min_acceptable_evaluation
 
     def getReward(self): # Return Number between 0-1 or False
-        return False
-
+        return self.website.evaluate()
 
 class Action():
+    # TODO: Define actions we can take in this space, could be genetic or based on the stats we gathered
     def __init__(self):
         pass
 
