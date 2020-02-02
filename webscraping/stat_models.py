312253c560adef660765418180a9eafdf5fc3a86
Eric Romrell <eric_romrell@byu.edu>
2020-02-02 10:37:38 -0700

    Got CSS working

--- webscraping/stat_models.py
+++ webscraping/stat_models.py
@@ -2,6 +2,8 @@ from typing import Tuple
 
 import tinycss2
 from bs4 import BeautifulSoup
+from tinycss2.ast import Declaration, Node
+
 from webscraping import web_scraper
 
 class HTML:
@@ -12,76 +14,18 @@ class HTML:
         self.ids = set([tag["id"] for tag in all_tags if tag.get("id") is not None])
         self.classes = set([klass for tag in all_tags if tag.get("class") is not None for klass in tag["class"]])
 
-class Rule:
-    def __init__(self, key: str, value: str):
-        self.key = key
-        self.value = value
-
-class RuleSet:
-    def __init__(self, selector: str, rules: [Rule]):
-        self.selector = selector
-        self.rules = rules
-
 class CSS:
     def __init__(self, html_soup: BeautifulSoup, print_issues = False):
-        # def parse_declarations(rule):
-        #     declarations = tinycss2.parse_declaration_list(rule.content, skip_comments = True, skip_whitespace = True)
-        #
-        #     dec_dict = {}
-        #     for declaration in declarations:
-        #         values = declaration.value
-        #         dec_dict[declaration.lower_name] = "".join([parse_value(value) for value in values]).strip()
-        #
-        #     return dec_dict
-        #
-        # def parse_value(value):
-        #     if value.type == "percentage":
-        #         return "{}%".format(value.value)
-        #     elif value.type == "dimension":
-        #         return "{}{}".format(value.value, value.unit)
-        #     elif value.type == "url":
-        #         return "url({})".format(value.value)
-        #     elif value.type == "ident":
-        #         return value.value
-        #     elif value.type == "hash":
-        #         return "#{}".format(value.value)
-        #     elif value.type == "number":
-        #         return str(value.value)
-        #     elif value.type == "string":
-        #         return value.value
-        #     elif value.type in ["whitespace", "literal"]:
-        #         return value.value
-        #     else:
-        #         if print_issues:
-        #             print("Unsupported value: {}".format(value))
-        #         return ""
-        #
-        # styles = html_soup.find_all("style")
-        # stats = {"tags": {}, "ids": {}, "classes": {}}
-        # for style in styles:
-        #     css = tinycss2.parse_stylesheet(style.text)
-        #     for rule_set in css:
-        #         if rule_set.type == "qualified-rule":
-        #             prelude = [token for token in rule_set.prelude if token.type != "whitespace"]
-        #             if len(prelude) == 1:
-        #                 subject = prelude[0]
-        #                 if subject.type == "ident":
-        #                     stats["tags"][subject.lower_value] = parse_declarations(rule_set)
-        #                 elif subject.type == "hash":
-        #                     stats["ids"][subject.value] = parse_declarations(rule_set)
-        #                 else:
-        #                     # TODO: Support other types?
-        #                     if print_issues:
-        #                         print("Unsupported prelude type: {}".format(subject.type))
-        #             elif len(prelude) == 2:
-        #                 if prelude[0].type == "literal" and prelude[0].value == "." and prelude[1].type == "ident":
-        #                     stats["classes"][prelude[1].lower_value] = parse_declarations(rule_set)
-        #             else:
-        #                 # TODO: Support other cases?
-        #                 if print_issues:
-        #                     print("Unsupported prelude size '{}': {}".format(len(prelude), prelude))
-        # TODO: Set these
-        self.rule_sets = []
+        self.print_issues = print_issues
+        self.rule_sets = {}
+        for style in html_soup.find_all("style"):
+            stylesheet = tinycss2.parse_stylesheet(style.text, skip_comments = True, skip_whitespace = True)
+            for rule_set in stylesheet:
+                selector = tinycss2.serialize(rule_set.prelude).strip()
+                if selector not in self.rule_sets:
+                    self.rule_sets[selector] = {}
+                # Update the current dictionary, overwriting conflicting keys
+                self.rule_sets[selector].update({declaration.lower_name: tinycss2.serialize(declaration.value).strip() for declaration in tinycss2.parse_declaration_list(rule_set.content, skip_comments = True, skip_whitespace = True)})
 
 class WebPage:
     def __init__(self, url: str):
--- webscraping/website_stat_collector_test.py
+++ webscraping/website_stat_collector_test.py
@@ -9,8 +9,22 @@ class StatCollectionTest(unittest.TestCase):
         self.assertCountEqual(html.ids, ["content", "title", "test-table", "awesomeBtn"])
         self.assertCountEqual(html.classes, ["center", "red", "image", "icon", "blue", "big"])
 
-    def full_test(self):
-        web_page = WebPage.from_url("test")
+    def test_css(self):
+        css = CSS(web_scraper.get_soup("test"), True)
+        self.assertCountEqual(css.rule_sets, ["body", "#content", "#title", "p", ".center", ".red", ".blue", ".big", "table#test-table", "td.image", "img.icon", "th, td"])
+        self.assertEqual(css.rule_sets["body"], {
+            "font-size": "18pt",
+            "background-image": "url(\"https://www.ezoic.com/wp-content/uploads/2016/05/website-background-min.jpg\")",
+            "background-repeat": "no-repeat"
+        })
+        self.assertEqual(css.rule_sets["#content"], {
+            "width": "50%",
+            "background-color": "#dddddddd",
+            "padding": "20px"
+        })
+
+    def test_full_web_page(self):
+        web_page = WebPage("test")
         self.assertIsInstance(web_page, WebPage)
         self.assertIsNotNone(web_page.html.soup)
         self.assertCountEqual(web_page.html.tags, ["html", "head", "title", "style", "body", "div", "h1", "p", "table", "thead", "tr", "th", "tbody", "td", "img", "button"])
